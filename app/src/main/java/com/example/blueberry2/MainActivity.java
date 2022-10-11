3df0da6df9e55ac7c0c90d6de49e8dc46e4e6853
_recordmystory <_recordmystory@abc.com>
2022-10-11 17:39:30 +0900

    파이어베이스 연동 최종

---
+++ app/src/main/java/com/example/blueberry2/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.blueberry2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.blueberry2.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
